b57c8dc40ddcd1e0fecb01cf8e8acf96413e4d40
Huzaifa Ali <80360413+huzaifali835@users.noreply.github.com>
2022-10-07 17:45:10 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import 'firebase/compat/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBJLAsaVyTk0eyd6TQvepV3SFlEy1LnQjo",
-    authDomain: "whatsapp-2-clone-5bc63.firebaseapp.com",
-    projectId: "whatsapp-2-clone-5bc63",
-    storageBucket: "whatsapp-2-clone-5bc63.appspot.com",
-    messagingSenderId: "866809205089",
-    appId: "1:866809205089:web:51f0c22731cdbce3a9b520",
-    measurementId: "G-KX7M8DFMG8"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   };
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
